8f8aedee200168cca035f842d15c57112b795bf0
Veselka Jiří <jiri.veselka@avast.com>
2020-01-13 16:46:20 +0100

    Changed printer used for circe encoding

--- slf4cats-circe/src/main/scala/slf4cats/encoders/circe/package.scala
+++ slf4cats-circe/src/main/scala/slf4cats/encoders/circe/package.scala
@@ -1,8 +1,9 @@
 package slf4cats.encoders
 
+import io.circe.Printer
 import slf4cats.api.LogEncoder
 
 package object circe {
   import io.circe.Encoder
-  implicit def convertEncoder[A](implicit circeEncoder: Encoder[A]): LogEncoder[A] = (a: A) => circeEncoder(a).toString
+  implicit def convertEncoder[A](implicit circeEncoder: Encoder[A]): LogEncoder[A] = (a: A) => Printer.noSpaces.print(circeEncoder(a))
 }
